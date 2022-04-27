2cfc6623b1dcc3afddf491423685f2429e1821e7
wackon <jeac86@gmail.com>
2022-04-27 13:57:25 -0500

    estado hasta capitulo 21

---
+++ pakiman/paki.js
@@ -0,0 +1,26 @@
+var imagenes = [];
+imagenes["Cauchin"] = "vaca.png";
+imagenes["Pokacho"] = "pollo.png";
+imagenes["Tocinauro"] = "cerdo.png";
+
+/* var cauchin = new Pakiman("Cauchin", 100, 30);
+var pokacho = new Pakiman("Pokacho", 80, 50);
+var tocinauro = new Pakiman("Tocinauro", 120, 40); */
+
+/* console.log(cauchin, Pakiman, tocinauro); */
+
+//pokacho.hablar();
+
+var coleccion = [];
+
+coleccion.push(new Pakiman("Cauchin", 100, 30));
+coleccion.push(new Pakiman("Pokacho", 80, 50));
+coleccion.push(new Pakiman("Tocinauro", 120, 40));
+
+for (var pakin of coleccion) {
+  pakin.mostrar();
+}
+
+for (var x in coleccion[0]) {
+  console.log(x);
+}
