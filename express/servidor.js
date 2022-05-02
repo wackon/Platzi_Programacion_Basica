c94e710f88d2bf7b57779291acfd6685944d281b
wackon <jeac86@gmail.com>
2022-05-02 15:06:23 -0500

    estado hasta el capitulo 24

---
+++ express/servidor.js
@@ -0,0 +1,17 @@
+var cafecito = require("express");
+var aplicacion = cafecito();
+
+aplicacion.get("/", inicio);
+aplicacion.get("/cursos", cursos);
+
+function inicio(peticion, resultado) {
+  resultado.send("Este es el <strong>home</strong>");
+}
+
+function cursos(peticion, resultado) {
+  resultado.send("Estos son los <strong>cursos</strong>");
+}
+
+aplicacion.listen(8989);
+
+//platzi.com/js
